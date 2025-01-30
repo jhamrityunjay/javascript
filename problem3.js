4b2f3616239bf1f27e6fa735ae679d4cf1f64ff2
jhamrityunjay <mrityunjayjha836@gmail.com>
2025-01-30 17:43:36 +0530

    problem3

---
+++ problem3.js
@@ -0,0 +1,8 @@
+// Problem 3: Print the sum of all the multiples of 3 from 0 to the given limit
+let x=12;
+let z=0;
+for(let y=1; y<=x; y++){
+    z=z+y*3;
+    console.log(`sum of ${z}`)
+}
+console.log(`sum of ${z}`);
